2d8ea4f7748c4a91d49e0f052febbe14496655d5
Stephen Mwangi <mukuastephen08@gmail.com>
2024-05-07 22:39:46 +0300

    Add files via upload
    Indentation in python

---
+++ greetings.py
@@ -0,0 +1,8 @@
+name="Stephen"
+if name=="Stephen":
+    print("Hello, Stephen")
+    print('How are you?')
+
+else:
+    print("Hello, stranger")
+    print("Who the hell are you and how may I help you")
